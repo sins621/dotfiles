7bd0442ad0c106ddb6bb3ecf8c5e85090407b54f
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-11-17 00:31:45 +0200

    update/battop-and-others

--- Neovim/.config/nvim/lua/sins/conf.lua
+++ Neovim/.config/nvim/lua/sins/conf.lua
@@ -10,3 +10,4 @@ vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+
