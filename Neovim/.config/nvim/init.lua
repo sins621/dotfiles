866d12303a3486beb2109849f40d5170f36bb6b7
sins621 <bradlycarpenterza@gmail.com>
2025-01-16 01:00:01 +0200

    update/nah-you-know

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -749,7 +749,6 @@ require('lazy').setup({
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
           ['<C-Space>'] = cmp.mapping.complete {},
-          ['<C-o>'] = cmp.mapping.complete {},
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
