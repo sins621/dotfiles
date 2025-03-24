f3ab7265b4366195bf904928c084065dc57e1fe5
Sins_621 <sinsmailza@gmail.com>
2025-03-25 00:06:01 +0200

    update/color-confgs

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -724,8 +724,7 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
-        -- completion = { completeopt = 'menu,menuone,noinsert', autocomplete = false },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = { completeopt = 'menu,menuone,noinsert', autocomplete = false },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
