bcb3674c817d6e26e1361e2b788b53d96eb3cc11
sins <sinsmailza@gmail.com>
2025-08-24 12:37:41 +0200

    float diagnostic vim

--- Neovim/.config/nvim/lua/keymaps.lua
+++ Neovim/.config/nvim/lua/keymaps.lua
@@ -5,6 +5,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Float Diagnostic
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Float diagnostic [E]rror message' })
+
 -- Exit Terminal
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
