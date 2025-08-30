e1311f5e69cf87aeed0516e6233590907775d529
sins <sins@DESKTOP-CFGB1ER.localdomain>
2025-08-30 16:57:14 +0200

    feat: mcp session

---
+++ Tmuxifier/.tmuxifier/layouts/mcp.session.sh
@@ -0,0 +1,22 @@
+
+session_root "~/Code/mcp_warp"
+
+if initialize_session "mcp"; then
+
+  new_window "dev"
+  run_cmd "cd mcp_golang && fresh"
+  
+  new_window "lazygit"
+  run_cmd "lazygit"
+
+  new_window "db"
+  run_cmd "lazysql"
+  
+  new_window "ai"
+  run_cmd "claude"
+
+  select_window 0
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
