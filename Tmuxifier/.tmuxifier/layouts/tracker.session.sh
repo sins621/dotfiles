07710573788ce76ba68030e2f7813c60952c3872
sins <sins@DESKTOP-CFGB1ER.localdomain>
2025-10-18 17:14:54 +0200

    chore: update tracker session

--- Tmuxifier/.tmuxifier/layouts/tracker.session.sh
+++ Tmuxifier/.tmuxifier/layouts/tracker.session.sh
@@ -3,9 +3,14 @@ session_root "~/Code/expense-tracker/"
 if initialize_session "tracker"; then
 
 	new_window "run"
-  run_cmd "cd back-end"
+  	run_cmd "cd back-end"
+	run_cmd "pnpm run test:watch"
 	split_h
-  run_cmd "cd front-end"
+  	run_cmd "cd front-end"
+	run_cmd "pnpm run test:watch"
+	split_v
+	run_cmd "cd front-end"
+	run_cmd "pnpm run storybook"
 	select_pane 0
 
 	new_window "lazygit"
