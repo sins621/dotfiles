a17c339c361500762c1defc3908e1b90beb5fd1f
sins <sinsmailza@gmail.com>
2026-02-03 23:31:15 +0200

    wip: home manager

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -5,20 +5,47 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-wsl.url = "github:nix-community/nixos-wsl/release-25.05";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }:
+  outputs = { home-manager, nixpkgs, nixos-wsl, ... }:
     let
       system = "x86_64-linux";
-    in
-    {
+      username = "sins";
+      specialArgs = { inherit username; };
+    in {
       nixosConfigurations = {
         wsl = nixpkgs.lib.nixosSystem {
           inherit system;
+          inherit specialArgs;
           modules = [
             nixos-wsl.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${username} = import;
+            }
             ./hosts/wsl
-            ./modules/cli.nix
+            ./modules/common
+            ./modules/cli
+          ];
+        };
+
+        sway = nixpkgs.lib.nixosSystem {
+          inherit system;
+          inherit specialArgs;
+          modules = [
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${username} = import;
+            }
+            ./hosts/sway
+            ./modules/common
+            ./modules/cli
           ];
         };
       };
--- nix-config/hosts/wsl/default.nix
+++ nix-config/hosts/wsl/default.nix
@@ -1,15 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   wsl.enable = true;
   wsl.defaultUser = "sins";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-  ];
 
   system.stateVersion = "25.05";
 }
