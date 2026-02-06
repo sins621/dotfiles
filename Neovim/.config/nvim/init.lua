370c08dc820ca7d69210eea632cb3a58765b7c1b
sins <sinsmailza@gmail.com>
2026-02-06 09:52:13 +0000

    vim auto switching

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -1,5 +1,25 @@
 vim.opt.number = true
 vim.o.splitright = true
 
-require('monokai-pro').setup({filter = 'spectrum'})
-vim.cmd.colorscheme('monokai-pro')
+require("auto-dark-mode").setup({
+  update_interval = 3000,
+  fallback = "dark",
+
+  set_dark_mode = function()
+    vim.opt.background = "dark"
+    require("monokai-pro").setup({
+      filter = "spectrum",
+    })
+    vim.cmd.colorscheme("monokai-pro")
+  end,
+
+  set_light_mode = function()
+    vim.opt.background = "light"
+    require("monokai-pro").setup({
+      filter = "light",
+    })
+    vim.cmd.colorscheme("monokai-pro")
+  end,
+})
+
+
--- nix-config/home/neovim.nix
+++ nix-config/home/neovim.nix
@@ -1,10 +1,24 @@
 { pkgs, ... }:
+
+let
+  auto-dark-mode-nvim = pkgs.vimUtils.buildVimPlugin {
+    pname = "auto-dark-mode-nvim";
+    version = "unstable";
+    src = pkgs.fetchFromGitHub {
+      owner = "f-person";
+      repo = "auto-dark-mode.nvim";
+      rev = "e300259ec777a40b4b9e3c8e6ade203e78d15881";
+      sha256 = "sha256-PhhOlq4byctWJ5rLe3cifImH56vR2+k3BZGDZdQvjng=";
+    };
+  };
+in
 {
   programs.neovim = {
     enable = true;
 
     plugins = with pkgs.vimPlugins; [
       monokai-pro-nvim
+      auto-dark-mode-nvim
     ];
   };
 }
